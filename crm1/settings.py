d6c68e998c5d822e3f15106a0cedb20fc41ee9c8
Kohei Fukada <k.fukada@kfukada-ltm2.internal.salesforce.com>
2020-06-09 00:28:49 +0900

    tutorial

--- crm1/settings.py
+++ crm1/settings.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     pass
 
-ALLOWED_HOSTS = ['deploy-quip-test.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm1',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -144,4 +147,5 @@ if not DEBUG:
     import django_heroku #追加
     django_heroku.settings(locals()) #追加
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
